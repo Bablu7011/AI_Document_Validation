a488cfb7b6ba73ed998c2ab1f70227cb22e58229
Bablu kumar <142781898+Bablu7011@users.noreply.github.com>
2025-07-23 10:37:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,5 @@ def validate():
     return 'File not uploaded', 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port=int(os.environ.get("PORT", 4000))
+    app.run(host="0.0.0.0", port=port)
